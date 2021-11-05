d6587ef4a78da184f2a1968a037ccdf375a598e1
Maude Charmetant <58000492+MaudeCharmetant@users.noreply.github.com>
2021-11-05 11:32:02 +0100

    Add files via upload

---
+++ my_functions/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# 
+#  This file is part of the my_functions package.
+# 
+#  my_functions is free software; you can redistribute it and/or modify
+#  it under the terms of the MIT License.
+# 
+#  my_functions is distributed in the hope that it will be useful,but 
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See 
+#  the provided copy of the MIT License for more details.
+
+"""my_functions package provide a function that multiply by 3 and a function that multiply a list by 3. 
+"""
+
+__version__ = "1.0"
+
+__bibtex__ = """
+"""
+
+from .my_functions import (example_1,example_2)
